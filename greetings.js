a2983503ea00482be8ae180821c6f617ceed44c7
Mxolisi <nyawose57@gmail.com>
2022-08-10 10:35:43 +0200

    savving changes

---
+++ greetings.js
@@ -0,0 +1,93 @@
+module.exports = function Greet() {
+  let name = ''
+  let language = ''
+  let greeted = {}
+
+  function setLanguage(lang) {
+    language = lang
+  }
+  function setName(username) {
+    name = username
+  }
+
+  function getName() {
+    return name
+  }
+  function getLanguage() {
+    return language
+  }
+
+
+  function getLanguage(name,language) {
+        
+    if (greeted[name] === undefined) {
+      greeted[name] = 1;
+    }
+    else {
+      greeted[name]++
+    }
+
+    if (language === 'english') {
+      return 'Hello, ' + name
+    }
+    if (language === 'isixhosa') {
+      return 'Molo, ' + name
+    }
+
+    if (language === 'isiZulu') {
+      return 'Sawubona, ' + name
+    }
+  }
+  
+
+  function errorMessage(name,language) {
+     
+    if (!language && !name) {
+        
+      return "Please enter your name and language"
+    }
+
+    else if (!name) {
+      return "Please enter your name"
+
+    }
+
+    else if (!language) {
+      return "Please select a language"
+    }
+     
+  }
+
+  function clearNames() {
+    greeted = {}
+  }
+
+  function countNames() {
+    
+    return Object.keys(greeted).length
+
+  }
+  function listofNames() {
+    return greeted
+  }
+
+  function resetButton() {
+    return "Names succesfully cleared!"
+
+  }
+
+  return {
+    resetButton,
+    listofNames,
+    clearNames,
+    errorMessage,
+    getName,
+    setLanguage,
+    getLanguage,
+    setName,
+    getName,
+    countNames,
+
+
+  }
+}
---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const flash = require('express-flash');
+const session = require('express-session');
+const exphbs = require("express-handlebars");
+const bodyParser = require('body-parser');
+const greetings = require('./greetings.js')([]);
+
+
+const app = express();
+
+app.use(session({
+  secret: "<add a secret string here>",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.render("index", {
+
+  });
+});
+
+
+app.post("/greet", function (req, res) {
+  let name = req.body.name
+  let language = req.body.language
+
+  if (!name || !language) {
+    let error = greetings.errorMessage(name, language)
+
+    res.render("index", {
+      error
+    })
+
+  } else {
+
+    let GreetAll = greetings.getLanguage(name, language)
+    let counter = greetings.countNames()
+
+    res.render("index", {
+      GreetAll, counter
+    })
+    
+  }
+});
+
+
+app.get("/clear", function (req, res) {
+  let GreetAll = greetings.resetButton()
+  greetings.clearNames()
+  res.render('index',{
+    GreetAll
+  });
+});
+
+
+
+app.get('/greeted', function (req, res) {
+
+  res.render('names', {
+    keynames: greetings.listofNames()
+  })
+});
+
+
+app.get("/counter/:name", function (req, res) {
+  let words = req.params.name
+  let person = greetings.listofNames()
+  var Text = ""
+  for (const name in person) {
+    if (name == words) {
+      let greetedTimes = person[name]
+      Text = `Hi , ${words} You have been greeted ${greetedTimes} times`
+    }
+  }
+  console.log(Text)
+  res.render('counter', {
+    Text
+  });
+
+});
+
+const PORT = process.env.PORT || 3008;
+
+app.listen(PORT, function () {
+  console.log('App starting on port', PORT);
+});
